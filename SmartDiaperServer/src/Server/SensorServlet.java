0c163091aec8bfcabd248fa8f375500a106eddfe
Boshra Nabaei <b.nabaei@gmail.com>
2017-11-28 18:00:42 +0700

    added sensor protection

--- SmartDiaperServer/src/Server/SensorServlet.java
+++ SmartDiaperServer/src/Server/SensorServlet.java
@@ -13,11 +13,16 @@ public class SensorServlet extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
+		Baby[] babies= MySQLBridge.msql.getBabies();
 		int[] babies_status = new int[5];
 		PrintWriter out = response.getWriter();
 
 		for (int i = 0; i < 5; i++) {
 			babies_status[i] = Integer.parseInt(request.getParameter("BABY" + (i + 1)));
+			
+			if(babies[i].status==1)
+				babies_status[i]=1;
+			
 			out.println("Baby " + i + " : " + babies_status[i]);
 		}
 
